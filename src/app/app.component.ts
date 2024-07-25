8bf3358ac5c7cc75a950baa23084f9d009b83bea
Shivamzee <hello.shivamzee@gmail.com>
2024-07-25 21:36:16 +0530

    learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employeeInfo';
+  constructor(private _tostr: ToastrService) {}
+  ngOnInit() {
+    Swal.fire({
+      title: '<strong>HTML <u>example</u></strong>',
+      icon: 'info',
+      html: `
+        You can use <b>bold text</b>,
+        <a href="#" autofocus>links</a>,
+        and other HTML tags
+      `,
+      showCloseButton: true,
+      showCancelButton: true,
+      focusConfirm: false,
+      confirmButtonText: `
+        <i class="fa fa-thumbs-up"></i> Great!
+      `,
+      confirmButtonAriaLabel: 'Thumbs up, great!',
+      cancelButtonText: `
+        <i class="fa fa-thumbs-down"></i>
+      `,
+      cancelButtonAriaLabel: 'Thumbs down',
+    });
+  }
 }
